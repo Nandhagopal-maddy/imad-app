b797d025c9ff385f4c9f8c0a785df26b8b9ca41b
Nandhagopal-maddy <30944565+Nandhagopal-maddy@users.noreply.github.com>
2017-08-12 20:07:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('my', function (req, res) {
-  res.send("my html is not working")
+app.get('/my', function (req, res) {
+  res.send("my html is not working");
 });
 
 app.get('/ui/style.css', function (req, res) {
