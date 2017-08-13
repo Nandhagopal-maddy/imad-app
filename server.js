a6bbc64bbfb9c24601487230de1122d5d7e0f294
Nandhagopal-maddy <30944565+Nandhagopal-maddy@users.noreply.github.com>
2017-08-13 18:41:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/my_html', function (req, res) {
 });
 
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main .js'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
