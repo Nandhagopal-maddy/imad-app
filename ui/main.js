d5a767857d8eb25b9a617cb1a8daa1d985bb073f
Nandhagopal-maddy <30944565+Nandhagopal-maddy@users.noreply.github.com>
2017-08-16 20:07:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,9 @@ var element = document.getElementById('main');
 element.innerHTML = 'jarvis';
  
  var img = document.getElementById('madi');
- img.onclick = function() {
+ img.onclick = function () {
 
     console.log('inside function');   
-    img.style.margin_left ='100px';     
+    img.style.marginLeft ='100px';     
  };
  
