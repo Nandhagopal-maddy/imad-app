0cc36830aaa92a37c3a59b2ae18a0237e9aba6b4
Nandhagopal-maddy <30944565+Nandhagopal-maddy@users.noreply.github.com>
2017-08-20 14:19:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,38 @@
-console.log('Loaded!');
 
-var element = document.getElementById('main');
-element.innerHTML = 'jarvis';
  
- var img = document.getElementById('madi');
- var marginLeft = 0;
- var marginRight = 0;
- function moveLeft(){
-     marginRight = marginRight +3;
-     img.style.marginLeft = marginRight+'px';
- }
- function moveRight (){
-     marginLeft = marginLeft + 3;
-     img.style.marginLeft = marginLeft + 'px';
- }
- img.onclick = function () {
-       if (marginLeft>=30) {
-        var interval2 = setInterval(moveLeft, 10);
-        if(marginLeft == 60)
-        {
-        marginLeft = marginLeft - 60;
-        }
-    }
-    var interval = setInterval (moveRight, 10);
-  
-        
+ var button = document.getElementById('counter');
+ 
+ button.onclick = function() {
+	 
+ var request = new XMLHttpRequest();
+ 
+ request.onreadystatechange = function() {
+	  
+	  if(request.status === XMLHttpRequest.DONE){
+		  
+	  if(request.status === 200) {
+			 
+	  var counter = Request.responseText;
+			 
+	  var span = document.getElementById('count');
+		 
+		 span.innerHTML = counter.toString();
+		 }
+	 }
  };
+ request.open('GET' ,'http://localhost:8080/counter', true);
+ request.send(null);
+ };
+  var nameinput = document.getElementById('name');
+  var names = nameinput.value;
+  var submit = document.getElementById('submit_btn');
+  submit.onclick = function() {
+	  var names = ['name1','name2','name3',];
+	  var list = ''; 
+	  for (var i =0; i< names.length; i++){
+		  list == '<li>'+ names[i]+'</li>';
+	  }
+	  var ul = document.getElementById('nameslist');
+	  ul.innerHTML = list;
+  };
  
